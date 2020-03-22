2f8e801d161d074872bad6f3088b24d06937454b
Carlos Sànchez <46933004+CarlosSanchez2019@users.noreply.github.com>
2020-03-22 13:42:32 -0300

    Update database.js

--- backend/app/database.js
+++ backend/app/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://carlos_sanchez:43543071Dd$@cluster0-hddky.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://carlos_sanchez:<Password>@cluster0-hddky.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify: false
